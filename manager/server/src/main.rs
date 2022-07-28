38134db2c30bda1d8de25e6bc830c66a3977343c
Tommy Volk <tvolk131@gmail.com>
2022-07-28 21:02:15 +0000

    Add basic http server to backend service

--- manager/server/src/main.rs
+++ manager/server/src/main.rs
@@ -1,6 +1,8 @@
 mod proto;
 mod service;
 
+use actix_web::{web, App, HttpServer};
+use futures::join;
 use proto::bolt_vend_api::v1::machine_service_server::MachineServiceServer;
 use proto::bolt_vend_api::v1::manager_service_server::ManagerServiceServer;
 use service::machine_service_impl::MachineServiceImpl;
@@ -9,14 +11,35 @@ use tonic::transport::Server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let port: u16 = 50052;
-    let address = format!("0.0.0.0:{}", port).parse().unwrap();
+    let grpc_port: u16 = 50052;
+    let grpc_address: std::net::SocketAddr = format!("0.0.0.0:{}", grpc_port).parse().unwrap();
 
-    println!("Starting server on port {}", port);
-    Server::builder()
+    let frontend_port: u16 = 8080;
+    let frontend_address: std::net::SocketAddr =
+        format!("0.0.0.0:{}", frontend_port).parse().unwrap();
+
+    println!("Starting server on port {}", grpc_port);
+    let grpc_server_future = Server::builder()
         .add_service(MachineServiceServer::new(MachineServiceImpl::new()))
         .add_service(ManagerServiceServer::new(ManagerServiceImpl::new()))
-        .serve(address)
-        .await?;
+        .serve(grpc_address);
+
+    println!("Starting frontend server on port {}", 8080);
+    let frontend_server_future =
+        HttpServer::new(|| App::new().route("/hello", web::get().to(|| async { "Hello World!" })))
+            .bind(frontend_address)?
+            .run();
+
+    let (grpc_server_status, frontend_server_status) =
+        join!(grpc_server_future, frontend_server_future);
+
+    if let Err(grpc_server_error) = grpc_server_status {
+        return Err(Box::from(grpc_server_error));
+    }
+
+    if let Err(frontend_server_error) = frontend_server_status {
+        return Err(Box::from(frontend_server_error));
+    }
+
     Ok(())
 }
